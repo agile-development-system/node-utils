5f52e5e356e3b47f4e28901fc9550d1c5c8ebf68
jinyang <961284688@qq.com>
2021-06-14 23:46:40 +0800

    refactor: 配置文件使用封装好的

--- babel.config.js
+++ babel.config.js
@@ -2,27 +2,6 @@
  * @Author: 锦阳
  * @Create: 2021年04月12日
  */
-module.exports = function (api) {
-    api.cache(true);
-    const presets = [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    node: '12',
-                },
-            },
-        ],
-    ];
-
-    const plugins = [
-        ['@babel/plugin-proposal-decorators', { legacy: true }],
-        ['@babel/plugin-proposal-class-properties', { loose: false }],
-        ['@babel/plugin-transform-modules-commonjs'],
-    ];
-
-    return {
-        presets,
-        plugins,
-    };
+module.exports = {
+    presets: ['@ads/babel-preset-base'],
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,5 @@
  * @Author: 锦阳
  * @Create: 2021年04月12日
  */
-const { series, src, dest } = require('gulp');
-const rimraf = require('rimraf');
-const babel = require('gulp-babel');
-const ts = require('gulp-typescript');
-const tsProject = ts.createProject('tsconfig.json');
-const merge = require('merge2');
-const run = require('gulp-cli');
-function clean(cb) {
-    rimraf('lib', cb);
-}
-function build(cb) {
-    const tsResult = src('src/**/*')
-        .pipe(tsProject());
-
-    merge([
-        tsResult.dts.pipe(dest('lib/types')),
-        tsResult.js.pipe(babel()).pipe(dest('lib')),
-    ]);
-    cb();
-}
-
-exports.default = series(clean, build);
-run();
+const gulpPreset = require('@ads/gulp-config-preset');
+exports.default = gulpPreset();
